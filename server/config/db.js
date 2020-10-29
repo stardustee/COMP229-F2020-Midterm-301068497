0382e0eb680981f8623c23888c6b1fcb31a8f1f2
sydney <sydneyhuang@rogers.com>
2020-10-29 15:51:20 -0400

    internal documentation

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 //module.exports = {local MongoDB deployment ->  "URI": "mongodb://localhost/books229"};
-
+/*Sydney Huang
+301068497
+COMP229-F2020-Mideterm[301068497]*/
 module.exports.URI = "mongodb+srv://Sydney:bbCgKr9jGWyed5Li@cluster0.d1dyx.mongodb.net/books_collection?retryWrites=true&w=majority";
 
