c8a061752bd3c6359f2e0cfb475cc0b349fe7c58
sydney <sydneyhuang@rogers.com>
2020-10-29 13:59:39 -0400

    First commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+//module.exports = {local MongoDB deployment ->  "URI": "mongodb://localhost/books229"};
+
+module.exports.URI = "mongodb+srv://Sydney:bbCgKr9jGWyed5Li@cluster0.d1dyx.mongodb.net/books_collection?retryWrites=true&w=majority";
+
